e59b16273a87068db72e3467f846374c5a853363
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2024-12-31 16:28:06 -0300

    Create form-vr.php

---
+++ form-vr.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GuiVRS</title>
+</head>
+<body>
+    <form action="enviar-form-vr.php" method="get">
+        digite o nome do vr <br>
+        <input type="text" name="nome"> <br>
+        digite as especificações do vr <br>
+        <input type="text" name="especificacoes"> <br>
+        digite o preço do vr <br>
+        <input type="text" name="preco"> <br>
+        digite a imagem do vr <br>
+        <input type="text" name="imagem"> <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
