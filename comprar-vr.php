71c54cec82b5b285b027b7b756767691aa41a7b0
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2024-12-31 16:24:28 -0300

    Create comprar-vr.php

---
+++ comprar-vr.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GuiVRS</title>
+</head>
+<body style="text-align: center;">
+    <h1>Aprovado</h1>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/8/84/Approved_img.png" alt="">
+    <p>Óculos Vr comprado com sucesso</p>
+</body>
+</html>
